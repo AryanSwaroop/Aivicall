eb9ab92af4d1626468292c30722823e5743c66f8
Aryan Swaroop <aryanswaroophere@gmail.com>
2024-06-08 17:11:22 +0530

    Deployment Adjust 7

--- client/src/Context/SocketProvider.jsx
+++ client/src/Context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-    const socket = useMemo( () => io("localhost:4000") , []);
+    const socket = useMemo( () => io("aivicall.onrender.com") , []);
 
     return (
         <SocketContext.Provider value={socket}>
