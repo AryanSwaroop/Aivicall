0dc3e77019b1a3c3c6d3f0b683f786c876d69ca5
Aryan Swaroop <aryanswaroophere@gmail.com>
2024-12-19 03:18:54 +0530

    fixed api endpoints 3

--- client/src/api/endpoints.js
+++ client/src/api/endpoints.js
@@ -6,10 +6,10 @@ const BASE_ORIGIN_FRONT = "aivicall.vercel.app";
 const BASE_ORIGIN_BACK = "aivicall.onrender.com";
 
 const API_ENDPOINTS = {
-  FRONT_URL: `${BASE_URL_FRONT}` ,
+  FRONT_URL: BASE_URL_FRONT ,
   GET_MEET_CODE: `${BASE_URL_BACK}/meet/meetCode`,
-  FRONT_ORIGIN : `${BASE_ORIGIN_FRONT}`,
-  BACK_ORIGIN : `${BASE_ORIGIN_BACK}`,
+  FRONT_ORIGIN : BASE_ORIGIN_FRONT,
+  BACK_ORIGIN : BASE_ORIGIN_BACK,
   BACK_CALLBACK : `${BASE_URL_BACK}/auth/google/callback`,
   AI_POST_ROUTE: `${BASE_URL_BACK}/ai/generate`
 };
