292131874536daefdb810505cee3049f39883a59
Aryan Swaroop <aryanswaroophere@gmail.com>
2025-01-12 11:29:04 +0530

    dev adjustment

--- server/DB/db.js
+++ server/DB/db.js
@@ -7,7 +7,7 @@ class db {
     }
 
     connect() {
-        mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017");
+        mongoose.connect(process.env.MONGO_URI);
     }
 
     close() {
