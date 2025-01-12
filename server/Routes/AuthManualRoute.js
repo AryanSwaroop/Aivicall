69973d822473e4b0290ea51581a539fde29a6e72
Aryan Swaroop <aryanswaroophere@gmail.com>
2025-01-12 16:06:36 +0530

    Depoloyement Adjustments

--- server/Routes/AuthManualRoute.js
+++ server/Routes/AuthManualRoute.js
@@ -64,6 +64,7 @@ router.post("/register", (req,res) => {
                     }
                     else {    
                      res.cookie("token" , token);
+                     res.status(200).json({message: "User registered successfully"});
                     }
                 })
             })
@@ -96,7 +97,10 @@ router.post("/login", async (req, res) => {
         }
 
         const token = jwt.sign({ userId: found._id }, process.env.JWT_SECRET, { expiresIn: '60s' });
+
         res.cookie("token", token);
+        res.status(200).json({message: "User registered successfully"});
+        
     } catch (err) {
         return res.status(500).json({ message: "Login failed", error: err.message });
     }
